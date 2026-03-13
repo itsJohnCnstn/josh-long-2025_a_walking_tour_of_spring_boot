40296ac44a832fb64515b812ff4c8d2fde84f5ea
Pavel Khankevich <johncnstn@gmail.com>
2026-03-13 14:59:02 +0100

    [integration] implement integration consumer. transform and handle the event

--- integration/src/main/java/com/johncnstn/integration/IntegrationApplication.java
+++ integration/src/main/java/com/johncnstn/integration/IntegrationApplication.java
@@ -1,7 +1,13 @@
 package com.johncnstn.integration;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.amqp.dsl.Amqp;
+import org.springframework.integration.core.GenericHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.json.JsonToObjectTransformer;
 
 @SpringBootApplication
 public class IntegrationApplication {
@@ -10,4 +16,27 @@ public class IntegrationApplication {
         SpringApplication.run(IntegrationApplication.class, args);
     }
 
+    @Bean
+    IntegrationFlow inboundIntegrationFlow(ConnectionFactory connectionFactory) {
+        var amqp = Amqp.inboundAdapter(connectionFactory, "adoptions");
+        return IntegrationFlow
+                .from(amqp)
+                .transform(new JsonToObjectTransformer(Dog.class))
+                // after transform it became dog from byte[]
+//                .handle((GenericHandler<byte[]>) (payload, headers) -> {
+//                    System.out.println(new String(payload));
+//                    headers.forEach((s, o) -> System.out.println(s + "=" + o));
+//                    return null;
+//                })
+                .handle((GenericHandler<Dog>) (payload, headers) -> {
+                    System.out.println(payload);
+                    headers.forEach((s, o) -> System.out.println(s + "=" + o));
+                    return null;
+                })
+                .get();
+    }
+
+}
+
+record Dog(int dogId) {
 }
